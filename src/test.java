7f82fd2351fb631b860a3bd89d9d95e71d8ba12f
tammy228 <saintpaulstammy@gmail.com>
2019-07-05 13:24:07 +0800

    delete test.java

--- src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class test {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n,i;
-		for(n = sc.nextInt(), i=0; i<n ; i++)
-			System.out.println("test");
-	}
-}
